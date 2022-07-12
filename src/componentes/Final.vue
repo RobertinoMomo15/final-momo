894e0a958b1ac70cd6b2f6b4d142d68a0130d5f6
RobertinoMomo15 <85981493+RobertinoMomo15@users.noreply.github.com>
2022-07-12 20:18:56 -0300

    Final

---
+++ src/componentes/Final.vue
@@ -0,0 +1,102 @@
+<template>
+
+  <section class="src-componentes-final">
+    <div class="jumbotron">
+      
+      <label>Color:</label>
+      <input type="text" v-model="color">
+      <hr>
+      <label>Fondo:</label>
+      <input type="text" v-model="fondo">
+      <hr>
+      <label>Size:</label>
+      <input type="number" v-model="size">
+      <hr>
+
+      <label>Texto:</label>
+      <input type="text" v-model="texto" :style="getEstilo">
+
+
+      <segundo v-show="texto " :texto="texto"  />
+
+    </div>
+
+    
+      <table class="table table-dark">
+        <tr>
+          <th>Pregunta</th>
+          <th>Respuesta</th>
+        </tr>
+        
+        <tr>
+          <td>1</td>
+          <td>B</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>C</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>B</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>D</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>D</td>
+        </tr>
+
+
+      </table>
+
+
+  </section>
+
+</template>
+
+<script>
+  
+  import segundo from './segundo.vue'
+
+  export default  {
+    name: 'src-componentes-final',
+    components:{
+      segundo
+    },
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        texto: "",
+        color:"",
+        fondo:"",
+        size: ""
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      getEstilo(){
+      return { 
+        'color': this.color,
+        'background-color': this.fondo,
+        'font-size': this.size + 'px'
+      }
+    }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-componentes-final {
+
+  }
+</style>
---
+++ src/componentes/segundo.vue
@@ -0,0 +1,42 @@
+<template>
+
+  <section class="src-componentes-segundo">
+    <h1>Cantidad de palabras: {{getPalabras}}</h1>
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-componentes-segundo',
+    props: ['texto'],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      getPalabras(){
+        var array = this.texto.trim().split(' ')
+        var cant = array.length
+        
+        return cant 
+      }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-componentes-segundo {
+
+  }
+</style>
